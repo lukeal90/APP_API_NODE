aaadd06a0eeb05dd2ca4a5c6d91fa141890e38fa
Costa Gonzalo <39466145@comunidadort.edu.ar>
2021-05-23 19:26:24 -0300

    Defino variables en ENV (database y movie externa api)

--- helpers/database.js
+++ helpers/database.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
-const user = 'admin'
-const pass = 'adminGLAN'
+const user = process.env.DATABASE_USERNAME;
+const pass = process.env.DATABASE_PASSWORD;
 
 const MONGODB_URI =`mongodb+srv://${user}:${pass}@cluster0.2cpgy.mongodb.net/atlas?retryWrites=true&w=majority`
 

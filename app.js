09793d0896c280ffd08340f74274178c75b91f31
Costa Gonzalo <39466145@comunidadort.edu.ar>
2021-05-23 17:07:53 -0300

    Creacion Scheme y Model de Pelicula

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
 import "./helpers/database.js";
-
-//console.log("Hola")
-
-/*
-import mongoose from 'mongoose';
-
-const user = 'admin'
-const pass = 'adminGLAN'
-
-const MONGODB_URI =`mongodb+srv://${user}:${pass}@cluster0.2cpgy.mongodb.net/atlas?retryWrites=true&w=majority`
-
-mongoose.connect(MONGODB_URI, 
-    {useUnifiedTopology: true, useNewUrlParser: true}
-)
-    .then(() => console.log('Database is connected'))
-    .catch(e => console.log(e))
-*/

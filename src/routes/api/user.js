d31c4f527470854c9140f49a177419e1e863d587
Lucas Laino <llaino@indec.gob.ar>
2021-06-05 15:37:18 -0300

    add auth

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -2,8 +2,6 @@ const { Router } = require('express');
 const router = Router();
 const schema = require('../../schemas/user');
 const validate = require('../middlewares/validate');
-
-
 const UserController = require('../../controllers/user');
 
 router.get('/adduser',validate(schema), UserController.addUser);
--- src/schemas/auth.js
+++ src/schemas/auth.js
@@ -1,7 +1,6 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
-    name: Joi.string().required(),
     email: Joi.string().required(),
     passwd: Joi.string().required()
 });

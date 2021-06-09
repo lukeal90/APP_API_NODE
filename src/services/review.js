cc0a99f15eb5b9b05008245191762d85ce868fd9
NicolasU94 <nicolas.s.ugarte@gmail.com>
2021-06-09 19:31:11 -0300

    changes done to the reviews controller

--- src/services/review.js
+++ src/services/review.js
@@ -5,16 +5,7 @@ const getReviewsId = async (id) =>{
 };
 
 const lastReviewsMovie = (movieId) => {
-    const reviews = ReviewModel.find({idPelicula: movieId}).exec();
-    let lastReviews;
-
-    if (reviews.length >= 5){ 
-     lastReviews = reviews.slice(Math.max(reviews.length - 5, 1));
-    }
-    else {
-       lastReviews = reviews;
-    }
-    return lastReviews;
+    return ReviewModel.find({idPelicula: movieId}).exec();
 }
 
 const addReview = (text, score, idP, idU) => {
